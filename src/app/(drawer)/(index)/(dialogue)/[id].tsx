43551124c9714c13ee29f8e65191c60d60423083
aidendk <aduceykline@gmail.com>
2024-07-21 16:08:04 -0400

    formatting of grammar and dialogue

--- src/app/(drawer)/(index)/(dialogue)/[id].tsx
+++ src/app/(drawer)/(index)/(dialogue)/[id].tsx
@@ -1,12 +1,14 @@
-import { Stack, useLocalSearchParams } from 'expo-router';
-import React from 'react';
-import { ScrollView, View, Text } from 'tamagui';
+import { Link, Stack, useLocalSearchParams } from 'expo-router';
+import React, { useState } from 'react';
+import { ScrollView, View, Text, YStack, Button, XStack } from 'tamagui';
 
 import units from '../../../../assets/data/units.json';
 
 import { Container, WhiteText } from '~/tamagui.config';
 
 const DialogueItem = () => {
+  const [showTranslation, setShowTranslation] = useState(false);
+
   const { id } = useLocalSearchParams();
 
   function getUnit() {
@@ -28,11 +30,44 @@ const DialogueItem = () => {
       <Stack.Screen options={{ gestureEnabled: false }} />
       <ScrollView backgroundColor="#2c2c44">
         <Container pt="$4" backgroundColor="#2c2c44" px="$3">
-          <View>
-            <WhiteText fontSize="$8" fontWeight="bold">
-              {studies?.unitInfo.dialogue.newDialogue}
+          <WhiteText fontSize="$8" fontWeight="bold">
+            {studies?.unitInfo.dialogue.newDialogue}
+          </WhiteText>
+        </Container>
+        <Container backgroundColor="#3a3a58" px="$4">
+          <YStack>
+            {studies?.unitInfo.dialogue.dialogueData?.map((dial) => {
+              return (
+                <YStack rowGap="$2" py="$3" key={dial.sentence}>
+                  <WhiteText fontWeight="bold" pl="$1" fontSize="$5">
+                    {dial.speaker}
+                  </WhiteText>
+                  <View backgroundColor="#27273a" p="$3" my="$1" borderRadius="$3">
+                    <WhiteText fontSize="$5">{dial.sentence}</WhiteText>
+                    {showTranslation && <WhiteText pt="$2">{dial.translation}</WhiteText>}
+                  </View>
+                </YStack>
+              );
+            })}
+          </YStack>
+        </Container>
+        <Container flexDirection="row" justifyContent="space-evenly">
+          <Link href="" asChild>
+            <Button size="$3" backgroundColor="#f63c7e" pressStyle={{ backgroundColor: '#ac2a58' }}>
+              <WhiteText fontWeight="bold" fontSize="$2">
+                Home
+              </WhiteText>
+            </Button>
+          </Link>
+          <Button
+            size="$3"
+            backgroundColor="#f63c7e"
+            pressStyle={{ backgroundColor: '#ac2a58' }}
+            onPress={() => setShowTranslation((value) => !value)}>
+            <WhiteText fontWeight="bold" fontSize="$2">
+              Show Translation
             </WhiteText>
-          </View>
+          </Button>
         </Container>
       </ScrollView>
     </>
--- src/app/(drawer)/(index)/(grammar)/[id].tsx
+++ src/app/(drawer)/(index)/(grammar)/[id].tsx
@@ -1,6 +1,6 @@
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { Link, Stack, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { ScrollView, View } from 'tamagui';
+import { Button, ScrollView, View } from 'tamagui';
 
 import units from '../../../../assets/data/units.json';
 
@@ -34,6 +34,27 @@ const GrammarItem = () => {
               {studies?.unitInfo.grammar.grammarTitle}
             </WhiteText>
           </View>
+        </Container>
+        <Container backgroundColor="#3a3a58" mx="$4">
+          <View>
+            <WhiteText>{studies?.unitInfo.grammar.grammarData}</WhiteText>
+          </View>
+        </Container>
+        <Container flexDirection="row" justifyContent="space-evenly">
+          <Link href="" asChild>
+            <Button size="$3" backgroundColor="#f63c7e" pressStyle={{ backgroundColor: '#ac2a58' }}>
+              <WhiteText fontWeight="bold" fontSize="$2">
+                Home
+              </WhiteText>
+            </Button>
+          </Link>
+          <Link href={`/(drawer)/(index)/(dialogue)/${studies?.unit}`} asChild>
+            <Button size="$3" backgroundColor="#f63c7e" pressStyle={{ backgroundColor: '#ac2a58' }}>
+              <WhiteText fontWeight="bold" fontSize="$2">
+                Continue to Dialogue
+              </WhiteText>
+            </Button>
+          </Link>
         </Container>
       </ScrollView>
     </>
